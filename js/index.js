7d867e47d9075ec9eedeff760b6ecc41b9f0349a
jay <jayyehice@gmail.com>
2022-09-06 13:29:10 +0800

    ser interval and max img size

--- js/index.js
+++ js/index.js
@@ -2,66 +2,73 @@
 function onQRCodeScanned(scannedText) {
     var scannedTextMemo = document.getElementById("scannedTextMemo");
     if (scannedTextMemo) {
-      scannedTextMemo.value = scannedText;
+        scannedTextMemo.value = scannedText;
     }
     var scannedTextMemoHist = document.getElementById("scannedTextMemoHist");
     if (scannedTextMemoHist) {
-      scannedTextMemoHist.value = scannedTextMemoHist.value + '\n' + scannedText;
+        scannedTextMemoHist.value = scannedTextMemoHist.value + '\n' + scannedText;
     }
-  }
+}
 
-  function provideVideo() {
+function provideVideo() {
     var n = navigator;
 
     if (n.mediaDevices && n.mediaDevices.getUserMedia) {
-      return n.mediaDevices.getUserMedia({
-        video: {
-          facingMode: "environment"
-        },
-        audio: false
-      });
+        return n.mediaDevices.getUserMedia({
+            video: {
+                facingMode: "environment",
+                minAspectRatio: 1.333,
+                minFrameRate: 60,
+                // width: {max: 300},
+                // heigth: {max: 300},
+            },
+            audio: false
+        });
     }
 
     return Promise.reject('Your browser does not support getUserMedia');
-  }
+}
 
-  function provideVideoQQ() {
-    return navigator.mediaDevices.enumerateDevices()
-      .then(function (devices) {
-        var exCameras = [];
-        devices.forEach(function (device) {
-          if (device.kind === 'videoinput') {
-            exCameras.push(device.deviceId)
-          }
-        });
+// function provideVideoQQ() {
+//     return navigator.mediaDevices.enumerateDevices()
+//         .then(function (devices) {
+//             var exCameras = [];
+//             devices.forEach(function (device) {
+//                 if (device.kind === 'videoinput') {
+//                     exCameras.push(device.deviceId)
+//                 }
+//             });
 
-        return Promise.resolve(exCameras);
-      }).then(function (ids) {
-        if (ids.length === 0) {
-          return Promise.reject('Could not find a webcam');
-        }
+//             return Promise.resolve(exCameras);
+//         }).then(function (ids) {
+//             if (ids.length === 0) {
+//                 return Promise.reject('Could not find a webcam');
+//             }
 
-        return navigator.mediaDevices.getUserMedia({
-          video: {
-            'optional': [{
-              'sourceId': ids.length === 1 ? ids[0] : ids[1]//this way QQ browser opens the rear camera
-            }]
-          }
-        });
-      });
-  }
+//             return navigator.mediaDevices.getUserMedia({
+//                 video: {
+//                     'optional': [{
+//                         'sourceId': ids.length === 1 ? ids[0] : ids[1]//this way QQ browser opens the rear camera
+//                     }]
+//                 }
+//             });
+//         });
+// }
 
-  //this function will be called when JsQRScanner is ready to use
-  function JsQRScannerReady() {
+//this function will be called when JsQRScanner is ready to use
+function JsQRScannerReady() {
     //create a new scanner passing to it a callback function that will be invoked when
     //the scanner succesfully scan a QR code
-    var jbScanner = new JsQRScanner(onQRCodeScanned);
-    //var jbScanner = new JsQRScanner(onQRCodeScanned, provideVideo);
+    // var jbScanner = new JsQRScanner(onQRCodeScanned);
+    var jbScanner = new JsQRScanner(onQRCodeScanned, provideVideo);
     //reduce the size of analyzed image to increase performance on mobile devices
-    jbScanner.setSnapImageMaxSize(300);
+    jbScanner.setSnapImageMaxSize(500);
     var scannerParentElement = document.getElementById("scanner");
     if (scannerParentElement) {
-      //append the jbScanner to an existing DOM element
-      jbScanner.appendTo(scannerParentElement);
+        //append the jbScanner to an existing DOM element
+        jbScanner.appendTo(scannerParentElement);
+
+        //間隔
+        jbScanner.setScanInterval(2000);
     }
-  }
+}
